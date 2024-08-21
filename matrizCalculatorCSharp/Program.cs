d6c62060bda2512f5f830ff1030845538528ad9d
(Alex)andre Cardoso <100371225+BIGTOST@users.noreply.github.com>
2024-08-21 03:04:40 +0100

    Update

--- matrizCalculatorCSharp/Program.cs
+++ matrizCalculatorCSharp/Program.cs
@@ -10,102 +10,123 @@ namespace matrizCalculator
             mainMenu();
         }
 
-        public static void mainMenu(){
+        public static void mainMenu()
+        {
             int seletor;
             Console.Write(
-                "Escolher a função\n"+
-                "\t 1 - Somar Matrizes\n"+
-                "\t 2 - Subtrair Matrizes\n"+
-                "\t 3 - Multiplicar Matrizes\n"+
+                "Escolher a função\n" +
+                "\t 1 - Somar Matrizes\n" +
+                "\t 2 - Subtrair Matrizes\n" +
+                "\t 3 - Multiplicar Matrizes\n" +
                 "\t 0 - encerrar programa\n"
             );
             seletor = Convert.ToInt32(Console.ReadLine());
-            switch(seletor){
+            switch (seletor)
+            {
                 case 1:
-                    int[,]  matriz1,
-                            matriz2;
-                    int col,
-                        row;
+                    //*declaração da matriz
+                    int[,]  matriz1 = criarMatriz(),
+                            matriz2 = criarMatriz();
 
-                    //*definindo tamanho e os parametros da primeira matriz
-                    Console.WriteLine("quantas colunas terá a 1ª matriz?");
-                    col = Convert.ToInt32(Console.ReadLine());
+                    mostrarMatriz(somarMatizes(matriz1, matriz2));
 
-                    Console.WriteLine("quantas Linhas terá o 1ª matriz?");
-                    row = Convert.ToInt32(Console.ReadLine());
-
-                    matriz1 = new int[col,row];
-
-                    for(int colCount = 0; colCount < col ; colCount++)
-                    {
-                        for(int rowCount = 0; rowCount < row; rowCount++)
-                        {
-                            Console.WriteLine($"Insira o valor da posição {colCount}x{rowCount} da 1ª matriz");
-                            matriz1[colCount,rowCount] = Convert.ToInt32(Console.ReadLine());
-                        }
-                    }
-
-                    //*definindo tamanho e os parametrosda segunda matriz
-                    Console.WriteLine("quantas colunas terá a 2ª matriz?");
-                    col = Convert.ToInt32(Console.ReadLine());
-
-                    Console.WriteLine("quantas Linhas terá o 2ª matriz?");
-                    row = Convert.ToInt32(Console.ReadLine());
-
-                    matriz2 = new int[col,row];
-            
-                    for(int colCount = 0; colCount < col ; colCount++)
-                    {
-                        for(int rowCount = 0; rowCount < row; rowCount++)
-                        {
-                            Console.WriteLine($"Insira o valor da posição {colCount}x{rowCount} da 2ª matriz");
-                            matriz2[colCount,rowCount] = Convert.ToInt32(Console.ReadLine());
-                        }
-                    }
-
-                    mostraMatriz(somarMatizes(matriz1, matriz2));
-                    
                     break;
                 case 2:
                     Console.WriteLine("Opção Dois");
                     break;
             }
         }
-        public static int[,] somarMatizes(int[,] matriz1, int[,] matriz2){
-            int[,] resultado = new int[matriz1.GetLength(0),matriz1.GetLength(1)];
+        public static int[,] criarMatriz()
+        {
+            int[,] matriz;
+
+            int col,
+                row;
+
+            //*definindo tamanho e os parametros da matriz
+            Console.WriteLine("quantas colunas terá a matriz?");
+            col = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("quantas Linhas terá a matriz?");
+            row = Convert.ToInt32(Console.ReadLine());
+
+            matriz = new int[col, row];
+
+            for (int colCount = 0; colCount < col; colCount++)
+            {
+                for (int rowCount = 0; rowCount < row; rowCount++)
+                {
+                    Console.WriteLine($"Insira o valor da posição {colCount}x{rowCount} da matriz");
+                    matriz[colCount, rowCount] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            return matriz;
+        }
+
+        public static void mostrarMatriz(int[,] matriz)
+        {
+            //* mostrar o resultado
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                Console.Write("[");
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    
+                    Console.Write($"{matriz[j, i]}\t");
+                }
+                Console.WriteLine(" ]");
+            }
+
+        }
+        public static int[,] somarMatizes(int[,] matriz1, int[,] matriz2)
+        {
+            int[,] resultado = new int[matriz1.GetLength(0), matriz1.GetLength(1)];
 
             //* verificar se as matrizes possuem o mesmo tamanho x e y
-            if(matriz1.GetLength(0)== matriz2.GetLength(0) && matriz1.GetLength(1) == matriz2.GetLength(1))
+            if (matriz1.GetLength(0) == matriz2.GetLength(0) && matriz1.GetLength(1) == matriz2.GetLength(1))
             {
-                for(int i = 0; i< matriz1.GetLength(0); i++)
+                for (int i = 0; i < matriz1.GetLength(0); i++)
                 {
-                    for(int j = 0; j < matriz1.GetLength(1); j++)
+                    for (int j = 0; j < matriz1.GetLength(1); j++)
                     {
                         //* realizar a soma
-                        resultado[i, j] = matriz1[i,j] + matriz2[i,j];
+                        resultado[i, j] = matriz1[i, j] + matriz2[i, j];
                     }
                 }
             }
             else
             {
-                 //*     caso não realizar um promp a dizer que não será possivel efeturar a operação
+                //*     caso não realizar um promp a dizer que não será possivel efeturar a operação
                 Console.WriteLine("Para ser somada as matrizes devem ter as mesmas dimenções");
-                return resultado = new int[0,0];
+                return resultado = new int[0, 0];
+            }
+            return resultado;
+        }
+        public static int[,] subtrairMatrizes(int[,] matriz1, int[,] matriz2)
+        {
+            int[,] resultado = new int[matriz1.GetLength(0), matriz1.GetLength(1)];
+
+            //* verificar se as matrizes possuem o mesmo tamanho x e y
+            if (matriz1.GetLength(0) == matriz2.GetLength(0) && matriz1.GetLength(1) == matriz2.GetLength(1))
+            {
+                for (int i = 0; i < matriz1.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matriz1.GetLength(1); j++)
+                    {
+                        //* realizar a soma
+                        resultado[i, j] = matriz1[i, j] - matriz2[i, j];
+                    }
+                }
+            }
+            else
+            {
+                //*     caso não realizar um promp a dizer que não será possivel efeturar a operação
+                Console.WriteLine("Para ser subtraida as matrizes devem ter as mesmas dimenções");
+                return resultado = new int[0, 0];
             }
             return resultado;
         }
 
-        public static void mostraMatriz(int[,] matriz)
-        {
-            //* mostrar o resultado
-            for(int i = 0; i< matriz.GetLength(0); i++){
-                Console.Write("[");
-                for(int j = 0; j < matriz.GetLength(1); j++)
-                {
-                    Console.Write($"{matriz[j,i]}\t");
-                }
-                Console.WriteLine(" ]");
-            }
-        }
     }
 }
