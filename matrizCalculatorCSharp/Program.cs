5530cbe783104dd7273f897ba81cc28dbb0d298a
(Alex)andre Cardoso <100371225+BIGTOST@users.noreply.github.com>
2024-08-24 14:48:08 +0100

    upades

--- matrizCalculatorCSharp/Program.cs
+++ matrizCalculatorCSharp/Program.cs
@@ -1,39 +1,72 @@
 ﻿using System;
+using System.ComponentModel.Design;
 
 namespace matrizCalculator
 {
     class Program
     {
+
+        
+
         public static void Main(string[] args)
         {
+            int seletor=1;
             Console.WriteLine("Program Start");
-            mainMenu();
+            do{
+                Console.Write(
+                    "Escolher a função\n" +
+                    "\t 1 - Somar Matrizes\n" +
+                    "\t 2 - Subtrair Matrizes\n" +
+                    "\t 3 - Multiplicar Matrizes\n" +
+                    "\t 0 - encerrar programa\n"
+                );
+                seletor = Convert.ToInt32(Console.ReadLine());
+                mainMenu(seletor);
+            }
+            while(seletor !=0);
         }
 
-        public static void mainMenu()
-        {
-            int seletor;
-            Console.Write(
-                "Escolher a função\n" +
-                "\t 1 - Somar Matrizes\n" +
-                "\t 2 - Subtrair Matrizes\n" +
-                "\t 3 - Multiplicar Matrizes\n" +
-                "\t 0 - encerrar programa\n"
-            );
-            seletor = Convert.ToInt32(Console.ReadLine());
+        public static void mainMenu(int seletor)
+        {   int[,]  matriz1,
+                    matriz2,
+                    resultado;
+            
             switch (seletor)
             {
                 case 1:
+                {
                     //*declaração da matriz
-                    int[,]  matriz1 = criarMatriz(),
-                            matriz2 = criarMatriz();
+                    matriz1 = criarMatriz();
+                    matriz2 = criarMatriz();
 
-                    mostrarMatriz(somarMatizes(matriz1, matriz2));
+                    Console.WriteLine("A soma das matrizes resulta em:");
+                    mostrarMatriz(somarMatrizes(matriz1, matriz2));
 
-                    break;
+                }
+                break;
                 case 2:
-                    Console.WriteLine("Opção Dois");
-                    break;
+                {
+                    matriz1 = criarMatriz();
+                    matriz2 = criarMatriz();
+                    Console.WriteLine("A subtração das matrizes resulta em:");
+                    resultado = subtrairMatrizes(matriz1, matriz2) ;
+                    if(resultado != null){
+                        mostrarMatriz(resultado);
+                    }
+                }
+                break;
+                case 3:
+                {
+                    matriz1 = criarMatriz();
+                    matriz2 = criarMatriz();
+                    Console.WriteLine("A subtração das matrizes resulta em:");
+                    resultado = multiplicarMatrizes(matriz1, matriz2);
+                    if(resultado != null){
+                        mostrarMatriz(resultado);
+                    }
+
+                }
+                break;
             }
         }
         public static int[,] criarMatriz()
@@ -79,7 +112,7 @@ namespace matrizCalculator
             }
 
         }
-        public static int[,] somarMatizes(int[,] matriz1, int[,] matriz2)
+        public static int[,] somarMatrizes(int[,] matriz1, int[,] matriz2)
         {
             int[,] resultado = new int[matriz1.GetLength(0), matriz1.GetLength(1)];
 
@@ -99,7 +132,7 @@ namespace matrizCalculator
             {
                 //*     caso não realizar um promp a dizer que não será possivel efeturar a operação
                 Console.WriteLine("Para ser somada as matrizes devem ter as mesmas dimenções");
-                return resultado = new int[0, 0];
+                return resultado = null;
             }
             return resultado;
         }
@@ -123,10 +156,33 @@ namespace matrizCalculator
             {
                 //*     caso não realizar um promp a dizer que não será possivel efeturar a operação
                 Console.WriteLine("Para ser subtraida as matrizes devem ter as mesmas dimenções");
-                return resultado = new int[0, 0];
+                return resultado = null;
             }
             return resultado;
         }
+         public static int[,] multiplicarMatrizes(int[,] matriz1, int[,] matriz2)
+        {
+            int[,] resultado = new int[matriz1.GetLength(0), matriz1.GetLength(1)];
 
+            //* verificar se as matrizes possuem o mesmo tamanho x e y
+            if (matriz1.GetLength(0) == matriz2.GetLength(0) && matriz1.GetLength(1) == matriz2.GetLength(1))
+            {
+                for (int i = 0; i < matriz1.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matriz1.GetLength(1); j++)
+                    {
+                        //* realizar a soma
+                        resultado[i, j] = matriz1[i, j] + matriz2[i, j];
+                    }
+                }
+            }
+            else
+            {
+                //*     caso não realizar um promp a dizer que não será possivel efeturar a operação
+                Console.WriteLine("Para ser somada as matrizes devem ter as mesmas dimenções");
+                return resultado = null;
+            }
+            return resultado;
+        }
     }
 }
